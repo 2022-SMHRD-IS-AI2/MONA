11f7d8a96433dcc9381303b21d2ef4207ddc817b
smhrd <smhrd@121.147.52.45>
2023-01-16 11:55:57 +0900

    2023-01-16 주문 주문 하는중

--- MONA/src/main/java/com/smhrd/controller/orderCon.java
+++ MONA/src/main/java/com/smhrd/controller/orderCon.java
@@ -5,6 +5,12 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.smhrd.model.OrderDAO;
+import com.smhrd.model.OrderVO;
+import com.smhrd.model.ProductDAO;
+import com.smhrd.model.ProductVO;
 
 
 public class orderCon extends HttpServlet {
@@ -12,16 +18,40 @@ public class orderCon extends HttpServlet {
 
 
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		request.setCharacterEncoding("UTF-8");
+
+		HttpSession session = request.getSession();
+	  	String u_id = (String) session.getAttribute("loginUser_id");
+	  	
 		String price=request.getParameter("price");
 		String prod_name =request.getParameter("prod_name");
 		String b=request.getParameter("b");
 		
 		int paid_amount=Integer.parseInt(price);
-		int prod_cnt=Integer.parseInt(b);
+		int cnt=Integer.parseInt(b);
 		
-		System.out.println("ddddddddddddddddddddddddddddddd"+prod_name);
-		System.out.println("ddddddddddddddddddddddddddddddd"+paid_amount);
-		System.out.println("ddddddddddddddddddddddddddddddd"+prod_cnt);
+//		System.out.println("상품이름"+prod_name);
+//		System.out.println("결제금액"+paid_amount);
+//		System.out.println("상품수량"+prod_cnt);
+//		System.out.println("주문자 아이디"+u_id);
+		
+
+		OrderVO vo= new OrderVO(u_id,paid_amount,cnt,prod_name); 
+		
+		int o_cnt = new OrderDAO().orderUpload(vo);
+		
+		if(o_cnt>0) {
+			System.out.println("주문 내역 업로드 성공");
+			response.sendRedirect("pay.jsp");
+		}else {
+			System.out.println("주문 내역 업로드 실패");
+			response.sendRedirect("Bucket.jsp");
+		}
+	
+		
+		
+		
 		
 	}
 
--- MONA/src/main/webapp/Bucket.jsp
+++ MONA/src/main/webapp/Bucket.jsp
@@ -180,10 +180,10 @@
   
     
         <div class="shoppingBag-bottom">
-        <form action="orderCon">
+        <form action="orderCon" action="post">
          <input hidden name="price" value="<%=sum+a%>">
           <input hidden name="prod_name" value="<%=vo.get(0).getProd_name()%>">
-           <input hidden name="prod_num" value="<%=b %>">
+           <input hidden name="b" value="<%=b%>">
             <button class="shoppingBag-bottom-btn" type="submit"  >
                     주문하기
                 </button>
